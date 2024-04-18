69af201dac0554dd6b8d9def20d35f9c7d5cd650
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-04-18 23:06:01 +0530

    error handling with chating ui improved

--- public/script/chatwith.js
+++ public/script/chatwith.js
@@ -1,34 +1,48 @@
-const socket=io();
-const chat=document.querySelector('textarea');
-const list=document.querySelector('#list');
-const U=document.querySelector('#user');
-const btnChat=document.querySelector('.btnChat');
-const to=document.querySelector('#to');
-
-
+const socket = io();
+const chat = document.querySelector("textarea");
+const list = document.querySelector("#list");
+const U = document.querySelector("#user");
+const btnChat = document.querySelector(".btnChat");
+const to = document.querySelector("#to");
+const textarea = document.getElementById("message");
 
 // const user={};
 
-socket.emit('login',{username:$('#username').val()});
-socket.on('loggedIn',()=>{console.log("Successfully LoggedIn")});
+socket.emit("login", { username: $("#username").val() });
+socket.on("loggedIn", () => {
+  console.log("Successfully LoggedIn");
+});
 
-btnChat.addEventListener('click',()=>{
-    socket.emit('messg',{messg:chat.value, to:to.value,username:$('#username').val()});
-    chat.value="";
-})
+btnChat.addEventListener("click", () => {
+  socket.emit("messg", {
+    messg: chat.value,
+    to: to.value,
+    username: $("#username").val(),
+  });
+  chat.value = "";
+  textarea.style.height = "50px";
+});
 
+socket.on("reciv", (data) => {
+  // console.log(data);
+  // const li=document.createElement('li');
+  // li.append(`${data.username} says :- ${data.messg}`);
+  // list.append(li);
+  const mes = document.createElement("div");
+  mes.append(`${data.username} says :- ${data.messg}`);
+  const user = U.innerText;
+  if (data.username === user) {
+    mes.setAttribute("class", "right");
+    console.log(user);
+  } else {
+    mes.setAttribute("class", "left");
+  }
+  list.append(mes);
+  list.scrollTop = list.scrollHeight;
+});
 
-socket.on('reciv',(data)=>{
-    // console.log(data);
-    // const li=document.createElement('li');
-    // li.append(`${data.username} says :- ${data.messg}`);
-    // list.append(li);
-    const mes=document.createElement('div');
-    mes.append(`${data.username} says :- ${data.messg}`);
-    const user=U.innerText
-    if(data.username===user){
-        mes.setAttribute("class","right")
-        console.log(user)
-    }
-    list.append(mes);
-})
+textarea.addEventListener("keyup", (e) => {
+  textarea.style.height = "50px";
+  let height = e.target.scrollHeight;
+  textarea.style.height = `${height}px`;
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,43 +1,63 @@
-const express=require("express");
-const router=express.Router();
-const User=require("../model/user");
-const passport=require("passport");
+const express = require("express");
+const router = express.Router();
+const User = require("../model/user");
+const passport = require("passport");
 
+router.get("/register", (req, res) => {
+  res.render("auth/signup");
+});
 
-
-router.get('/register',(req,res)=>{
-    res.render('auth/signup');
-})
-
-router.post('/register',async(req,res)=>{
-    const user=new User({username:req.body.username,email:req.body.email});
-    await User.register(user,req.body.password);
-    res.redirect('/login');
-})
-
-router.get('/login',(req,res)=>{
-    res.render('auth/login');
-})
-
-router.post('/login',passport.authenticate('local',{
-    failureRedirect:'/login'    
-}),(req,res)=>{
-    res.redirect('/');
-})
-
-router.get('/logout',(req,res)=>{
-    req.logOut(()=>{
-        res.redirect('/login');
+router.post("/register", async (req, res) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
     });
-})
+    await User.register(user, req.body.password);
+    req.flash("success", "Registered Successfully login and start");
+    passport.authenticate("local", {
+      failureRedirect: "/register",
+      failureFlash: true,
+    });
+    res.redirect("/login");
+  } catch (e) {
+    req.flash("error", `${e.message}`);
+    res.redirect("/register");
+  }
+});
 
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
 
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    try {
+      console.log(await User.find({ username: "shubham" }));
+      req.flash("success", `Welcome Back!! ${req.user.username}`);
+      res.redirect("/");
+    } catch (e) {
+      req.flash("error", `${e.message}`);
+      res.redirect("/login");
+    }
+  }
+);
 
+router.get("/logout", (req, res) => {
+  try {
+    req.logOut(() => {
+      req.flash("success", "logged out successfully");
+      res.redirect("/login");
+    });
+  } catch (e) {
+    req.flash("error", `${e.message}`);
+    res.redirect("/error");
+  }
+});
 
-
-
-
-
-
-
-module.exports=router;
+module.exports = router;
