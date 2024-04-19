72adbd454edbf83d41f527151877bf16b4508ff8
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-04-19 18:00:40 +0530

    improve login procedure

--- routes/auth.js
+++ routes/auth.js
@@ -10,10 +10,10 @@ router.get("/register", (req, res) => {
 router.post("/register", async (req, res) => {
   try {
     const user = new User({
-      username: req.body.username,
-      email: req.body.email,
+      username: req.body.username.trim(),
+      email: req.body.email.trim(),
     });
-    await User.register(user, req.body.password);
+    await User.register(user, req.body.password.trim());
     req.flash("success", "Registered Successfully login and start");
     passport.authenticate("local", {
       failureRedirect: "/register",
@@ -30,23 +30,54 @@ router.get("/login", (req, res) => {
   res.render("auth/login");
 });
 
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  async (req, res) => {
-    try {
-      console.log(await User.find({ username: "shubham" }));
-      req.flash("success", `Welcome Back!! ${req.user.username}`);
-      res.redirect("/");
-    } catch (e) {
-      req.flash("error", `${e.message}`);
-      res.redirect("/login");
+// Login Route
+router.post("/login", async (req, res) => {
+  try {
+    const username = req.body.username.trim(); // Trim whitespace from username
+    req.body.username = req.body.username.trim(); // Trim whitespace from username
+
+    const password = req.body.password.trim(); // Trim whitespace from password
+    req.body.password = req.body.password.trim(); // Trim whitespace from password
+
+    // Validate username
+    if (!username) {
+      throw new Error("Username is required.");
     }
+    if (!/\S/.test(username)) {
+      throw new Error("Username cannot be whitespace only.");
+    }
+
+    // Validate username
+    if (!password) {
+      throw new Error("Password is required.");
+    }
+    if (!/\S/.test(password)) {
+      throw new Error("Password cannot be whitespace only.");
+    }
+
+    // Authenticate user
+    passport.authenticate("local", (err, user, info) => {
+      if (err) {
+        throw err;
+      }
+
+      if (!user) {
+        req.flash("error", "Incorrect username or password.");
+        return res.redirect("/login");
+      }
+      req.logIn(user, (err) => {
+        if (err) {
+          throw err;
+        }
+        req.flash("success", `Welcome Back!! ${username}`);
+        return res.redirect("/");
+      });
+    })(req, res);
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/login");
   }
-);
+});
 
 router.get("/logout", (req, res) => {
   try {
