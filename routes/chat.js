8585de327b86de89062e53dcb9950c84c81f16ac
ShubhamP528 <shubham528prajapati@gmail.coom>
2024-05-07 15:08:01 +0530

    more enhanced chating exprience and new landing page added

--- routes/chat.js
+++ routes/chat.js
@@ -7,15 +7,27 @@ const users=require('../index');
 
 
 router.post('/newChat',isLoggedIn,async(req,res)=>{
-    const obj = await User.find();
-    res.render('chat/allUser',{obj,users});
+    try{
+        const obj = await User.find();
+        res.render('chat/allUser',{obj,users});
+    }
+    catch(err){
+        req.flash('error',`${err.message}`)
+        console.log(err);
+    }
+
 })
 router.post('/chatwith/:username',isLoggedIn,(req,res)=>{
-    const {username}=req.params;
-    const name=req.user;
-    // console.log(username);
-    // console.log(name);
-    res.render('chat/chatwith',{username,name});
+    try{
+        const {username}=req.params;
+        const name=req.user;
+        // console.log(username);
+        // console.log(name);
+        res.render('chat/chatwith',{username,name});
+    }catch(e){
+        req.flash('error',`${err.message}`)  
+    }
+
 })
 
 
